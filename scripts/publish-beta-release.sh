c8e0f3da32e24899f73d36636c5e7977ec0c6e4d
Mircea Nistor <mirceanis@gmail.com>
2020-11-30 16:56:18 +0100

    build(ci): third attempt to avoid pushing changelog files and package files

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -5,4 +5,5 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 cd "$PROJECT_DIR"
 
 npx lerna version --conventional-commits --conventional-prerelease --force-publish --include-merged-tags --no-git-tag-version --no-push --create-release github --preid beta --yes
-npx lerna publish from-git --canary --pre-dist-tag beta --no-git-reset --yes
+git commit -a -m "chore(beta-release): don't push me"
+npx lerna publish from-package --canary --pre-dist-tag beta --no-git-reset --yes
