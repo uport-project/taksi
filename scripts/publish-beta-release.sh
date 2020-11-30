f479350a8bda6447917f1f70bd6407584eacead5
Mircea Nistor <mirceanis@gmail.com>
2020-11-30 14:59:47 +0100

    chore(build): clean release scripts

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -5,4 +5,4 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 cd "$PROJECT_DIR"
 
 npx lerna version --conventional-commits --conventional-prerelease --force-publish --include-merged-tags --create-release github --preid beta --yes
-npx lerna publish from-git --pre-dist-tag beta --no-git-reset --include-merged-tags --yes
+npx lerna publish from-git --pre-dist-tag beta --no-git-reset --yes
