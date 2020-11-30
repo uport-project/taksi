514b17e779c4be356e8b3ddbbe24df9f9ec16640
Mircea Nistor <mirceanis@gmail.com>
2020-11-30 14:34:47 +0100

    feat: generate gh release

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -5,4 +5,4 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 cd "$PROJECT_DIR"
 
 npx lerna version --conventional-commits --conventional-graduate --include-merged-tags --create-release github --yes
-npx lerna publish from-git --no-git-reset --include-merged-tags --yes
+npx lerna publish from-git --no-git-reset --include-merged-tags --create-release github --yes
