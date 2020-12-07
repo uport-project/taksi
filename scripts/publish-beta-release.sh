4669a33fbdac0cba50da6aad294065827aba6070
Mircea Nistor <mirceanis@gmail.com>
2020-12-07 14:35:26 +0100

    chore: manually tag and push beta release

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -6,6 +6,8 @@ cd "$PROJECT_DIR"
 
 npx lerna version --conventional-commits --conventional-prerelease --force-publish --include-merged-tags --no-git-tag-version --create-release github --preid beta --yes
 # tag the release but don't update package.json or changelog files
-git tag $(echo "console.log(require('./lerna.json').version)" | node) && git push --tags
+git add lerna.json
+git commit -m "chore(beta-release): tag new release [skip ci]"
+git tag $(echo "console.log(require('./lerna.json').version)" | node) && git push -u origin beta --tags
 git commit -a -m "chore(beta-release): don't push me"
-npx lerna publish from-package --canary --pre-dist-tag beta --no-git-reset --yes
+npx lerna publish from-git --canary --pre-dist-tag beta --no-git-reset --yes
