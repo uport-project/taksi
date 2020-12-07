8c6d87413f072d23e1b9213c12ba4ccd6e0bd2c1
Mircea Nistor <mirceanis@gmail.com>
2020-12-07 16:22:32 +0100

    feat: simulate feature development

--- packages/kerno/src/kerno.ts
+++ packages/kerno/src/kerno.ts
@@ -2,7 +2,7 @@ import { IKerno, TKerno } from './types/KernoAgent'
 import { KernoPlugin, PluginMethodMap } from './types/KernoPlugin'
 
 /**
- * Core (kerno) config options
+ * Core config options
  * @public
  */
 export interface KernoOptions {
