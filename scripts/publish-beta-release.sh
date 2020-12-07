86ed6cd38a0f4aac8fec6715831d2927039ad164
Mircea Nistor <mirceanis@gmail.com>
2020-12-07 17:52:12 +0100

    chore(ci): add prerelease notes using gh actions

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
 cd "$PROJECT_DIR"
 
-#npx lerna version prerelease --force-publish --no-git-tag-version --preid beta --yes
 npx lerna publish --force-publish --no-git-tag-version --canary --preid beta --yes --pre-dist-tag beta --no-git-reset --create-release github
-## tag the release but don't update package.json or changelog files
-#git add lerna.json
-#git commit -m "chore(beta-release): tag new release [skip ci]"
+## tag the prerelease with "v${version}"
 export TAGNAME="v$(echo "console.log(require('./packages/kerno/package.json').version)" | node)"
 git tag $TAGNAME && git push origin $TAGNAME
-##git commit -a -m "chore(beta-release): don't push me"
