b2cd9dd672712cfc323e0b7ea3dfa6b30735195f
Mircea Nistor <mirceanis@gmail.com>
2020-12-07 18:55:12 +0100

    chore(ci): simple chore

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -6,5 +6,4 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 
 cd "$PROJECT_DIR"
 
-npx lerna version --conventional-commits --conventional-graduate --include-merged-tags --create-release github --yes
-npx lerna publish from-git --no-git-reset --yes
+npx lerna publish --conventional-commits --include-merged-tags --no-git-reset  --create-release github --yes
