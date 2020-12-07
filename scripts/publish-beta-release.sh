eafd4aede0b01adf1276b0712c58965bc9622ca8
Mircea Nistor <mirceanis@gmail.com>
2020-12-07 16:52:39 +0100

    chore(ci): attempt 3 at one-liner release

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -5,7 +5,7 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 cd "$PROJECT_DIR"
 
 #npx lerna version prerelease --force-publish --no-git-tag-version --preid beta --yes
-npx lerna publish --no-git-tag-version --canary --preid beta --yes --pre-dist-tag beta --no-git-reset --create-release github
+npx lerna publish --force-publish --no-git-tag-version --canary --preid beta --yes --pre-dist-tag beta --no-git-reset --create-release github
 ## tag the release but don't update package.json or changelog files
 #git add lerna.json
 #git commit -m "chore(beta-release): tag new release [skip ci]"
