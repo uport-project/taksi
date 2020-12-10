5e37d7b53055db527c97a34c58790645a7382d4c
Mircea Nistor <mirceanis@gmail.com>
2020-12-10 18:14:42 +0100

    chore(ci): new prerelease approach

--- scripts/publish-beta-release.sh
+++ scripts/publish-beta-release.sh
@@ -6,7 +6,10 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.
 
 cd "$PROJECT_DIR"
 
-npx lerna publish --force-publish --no-git-tag-version --canary --preid beta --yes --pre-dist-tag beta --no-git-reset --create-release github
-## tag the prerelease with "v${version}"
-export TAGNAME="pre$(echo "console.log(require('./packages/kerno/package.json').version)" | node)"
-git tag $TAGNAME && git push origin $TAGNAME
+# npx lerna publish --force-publish --no-git-tag-version --canary --preid beta --yes --pre-dist-tag beta --no-git-reset --create-release github
+npx lerna publish --force-publish --canary --preid beta --pre-dist-tag latest --create-prerelease github --tag-version-prefix 'pre' --amend --yes
+# ## tag the prerelease with "v${version}"
+# export TAGNAME="pre$(echo "console.log(require('./packages/kerno/package.json').version)" | node)"
+# git tag $TAGNAME && git push origin $TAGNAME
+
+# npx lerna publish --force-publish --canary --preid beta --pre-dist-tag latest --create-prerelease github --yes
