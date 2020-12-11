d763b3c8cf99853c15447fc62f3719fa5fca59b8
Mircea Nistor <mirceanis@gmail.com>
2020-12-11 21:24:38 +0100

    chore(ci): manually use NPM_TOKEN

--- scripts/publish-canary-release.sh
+++ scripts/publish-canary-release.sh
@@ -5,7 +5,9 @@ set -o pipefail
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 cd "$PROJECT_DIR"
 
+git remote set-url origin https://uport-project:$GH_TOKEN@github.com/uport-project/taksi.git
 git config user.email $GH_EMAIL
 git config user.name $GH_USER
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 
 npx lerna publish --conventional-prerelease --force-publish --canary --no-git-reset --no-git-tag-version --include-merged-tags --preid next --pre-dist-tag next --yes
