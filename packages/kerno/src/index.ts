7d6813b478359d667947823e0e57d347c9f3e47b
Mircea Nistor <mirceanis@gmail.com>
2020-12-11 10:43:33 +0100

    feat(kerno): simulate conflicting feature 345

--- packages/kerno/src/index.ts
+++ packages/kerno/src/index.ts
@@ -2,6 +2,8 @@
  * Provides {@link @taksi/kerno#Kerno} implementation and defines {@link @taksi/kerno#KernoOptions},
  * {@link @taksi/kerno#KernoPlugin} interfaces
  *
+ * conflicting change in beta
+ *
  * @packageDocumentation
  */
 export { Kerno, KernoOptions, createKerno } from './kerno'
