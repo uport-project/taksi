3d02e31b383688b24bdd3d2ea6ce0bc4f2f1a124
Mircea Nistor <mirceanis@gmail.com>
2020-12-11 21:45:09 +0100

    chore(ci): gah!!

--- scripts/publish-canary-release.sh
+++ scripts/publish-canary-release.sh
@@ -5,9 +5,6 @@ set -o pipefail
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 cd "$PROJECT_DIR"
 
-git remote set-url origin https://uport-project:$GH_TOKEN@github.com/uport-project/taksi.git
-git config user.email $GH_EMAIL
-git config user.name $GH_USER
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+npm whoami
 
 npx lerna publish --conventional-prerelease --force-publish --canary --no-git-reset --no-git-tag-version --include-merged-tags --preid next --pre-dist-tag next --yes
