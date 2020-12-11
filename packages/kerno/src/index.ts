8a1d8b28f611a1bda31a0d666dfa6ee7123a8d3d
Mircea Nistor <mirceanis@gmail.com>
2020-12-11 10:45:25 +0100

    fix(kerno): simulate conflicting fix 3456

--- packages/kerno/src/index.ts
+++ packages/kerno/src/index.ts
@@ -2,6 +2,7 @@
  * Provides {@link @taksi/kerno#Kerno} implementation and defines {@link @taksi/kerno#KernoOptions},
  * {@link @taksi/kerno#KernoPlugin} interfaces
  *
+ * conflicting change in master
  * @packageDocumentation
  */
 export { Kerno, KernoOptions, createKerno } from './kerno'
