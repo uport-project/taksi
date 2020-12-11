37f47caca013453fe5268ffefb1f672514194b43
Mircea Nistor <mirceanis@gmail.com>
2020-12-11 21:09:29 +0100

    chore(ci): fix git user setup for release

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -3,7 +3,9 @@ set -e
 set -o pipefail
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
-
 cd "$PROJECT_DIR"
+
+git config user.email $GH_EMAIL
+git config user.name $GH_USER
 
 npx lerna publish --conventional-commits --include-merged-tags --no-git-reset  --create-release github --yes
