9fadc9408b090134622cb1a72a30f9e25e3f715a
Mircea Nistor <mirceanis@gmail.com>
2020-12-15 13:47:18 +0100

    fix(vorto): simulate a fix (#15)

--- packages/vorto/src/index.ts
+++ packages/vorto/src/index.ts
@@ -1,6 +1,8 @@
 /**
  * Provides {@link @taksi/vorto#Vorto} implementation and defines {@link @taksi/vorto#VortoMethods} plugin interface
  *
+ * // nop
+ *
  * @packageDocumentation
  */
 export { Vorto, VortoMethods } from './vorto'
